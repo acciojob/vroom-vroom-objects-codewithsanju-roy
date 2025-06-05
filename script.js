3e836302c44aa06909364dc3b2912e197de291bf
codewithsanju-roy <sanjuroy806@gmail.com>
2025-06-05 18:19:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
-// Complete the js code
 function Car(make, model) {
-	this.make = make;
-	this.model = model;
-	Car.prototype.getMakeModel = function() {
-		return .this`${this.make}${this.model}`
-	}
-}
+      this.make = make;
+      this.model = model;
+    }
 
-function SportsCar(make, model, topSpeed) {
-	Car.call(this,make, model);
-	this.topSpeed= topSpeed;
-}
-SportsCar.prototype = Object.create(Car.prototype);
-SportsCar.prototype.constructor = SportsCar; 
-SportsCar.prototype.getTopSpeed = function() {
-  return this.topSpeed;
-}
+    Car.prototype.getMakeModel = function () {
+      return `${this.make} ${this.model}`;
+    };
 
-// Do not change the code below
-window.Car = Car;
-window.SportsCar = SportsCar;
+    function SportsCar(make, model, topSpeed) {
+      Car.call(this, make, model); // Inherit Car properties
+      this.topSpeed = topSpeed;
+    }
+
+
+    SportsCar.prototype = Object.create(Car.prototype);
+    SportsCar.prototype.constructor = SportsCar;
+
+    SportsCar.prototype.getTopSpeed = function () {
+      return this.topSpeed;
+    };
+
+
+    function createCar() {
+      const make = document.getElementById('make').value;
+      const model = document.getElementById('model').value;
+      const topSpeed = Number(document.getElementById('topSpeed').value);
+
+      const car = new SportsCar(make, model, topSpeed);
+
+      const output = `
+        <strong>Make & Model:</strong> ${car.getMakeModel()}<br>
+        <strong>Top Speed:</strong> ${car.getTopSpeed()} kmph
+      `;
+
+      document.getElementById('output').innerHTML = output;
+    }
